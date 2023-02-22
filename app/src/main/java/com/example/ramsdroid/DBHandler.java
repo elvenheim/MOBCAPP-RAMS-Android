b7939c7d7c321d5363e00829cd94c8b584994cc6
MindBreak11 <johnmavric11@gmail.com>
2023-02-22 16:40:38 +0800

    marj update

--- app/src/main/java/com/example/ramsdroid/DBHandler.java
+++ app/src/main/java/com/example/ramsdroid/DBHandler.java
@@ -1,4 +1,28 @@
 package com.example.ramsdroid;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 public class DBHandler {
+
+    //name of db
+    private static  final  String DB_Name = "APCIS_Users";
+
+    //name of table
+    private  static  final  String TName = "Users";
+
+    //table col
+    private static final String Email = "Email";
+    private static final String Password = "Password";
+    private static final String givenName = "givenName";
+    private static final String Mid = "Mid";
+    private static final String lastName = "lastName";
+
+    public void onCreate(SQLiteDatabase db, String userEmail){
+
+        String query = "SELECT * FROM " + DB_Name + " WHERE " + Email + " = " + userEmail;
+
+    }
+
+
 }
