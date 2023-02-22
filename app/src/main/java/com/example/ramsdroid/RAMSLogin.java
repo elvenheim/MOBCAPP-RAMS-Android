9603e8ed481b9a8151a644da680671a1653d8db3
MindBreak11 <johnmavric11@gmail.com>
2023-02-22 19:51:30 +0800

    marj update

--- app/src/main/java/com/example/ramsdroid/RAMSLogin.java
+++ app/src/main/java/com/example/ramsdroid/RAMSLogin.java
@@ -3,8 +3,14 @@ package com.example.ramsdroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class RAMSLogin extends AppCompatActivity {
+
+    EditText email, pass;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
