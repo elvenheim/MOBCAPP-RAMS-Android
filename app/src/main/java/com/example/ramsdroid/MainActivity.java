baf04d1049e5518af4fe8bcf8a397ec052ef1897
kin10101 <ijoacquin@gmail.com>
2023-02-22 16:55:42 +0800

    initial home page

--- app/src/main/java/com/example/ramsdroid/MainActivity.java
+++ app/src/main/java/com/example/ramsdroid/MainActivity.java
@@ -1,14 +1,19 @@
-//package com.example.ramsdroid;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.rams_home);
-//    }
-//}
+package com.example.ramsdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rams_home);
+
+        //if isUsernameValid() && isPasswordValid(){
+            //then switch intent to home screen
+
+        //}
+    }
+}
