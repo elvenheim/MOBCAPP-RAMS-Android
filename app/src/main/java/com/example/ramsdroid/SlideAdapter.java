f4d715a0cad5c578dd20b94fca812e6cfece10af
kin10101 <ijoacquin@gmail.com>
2023-02-22 17:56:30 +0800

    image slider initial stage

---
+++ app/src/main/java/com/example/ramsdroid/SlideAdapter.java
@@ -0,0 +1,12 @@
+//package com.example.ramsdroid;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//import makeramen.roundedimageview.RoundedImageView;
+//
+//public class SlideAdapter {
+//
+//    class SlideViewHolder extends RecyclerView.ViewHolder{
+//        private RoundedImageView imageView;
+//
+//    }
+//}
---
+++ app/src/main/java/com/example/ramsdroid/SlideItem.java
@@ -0,0 +1,5 @@
+package com.example.ramsdroid;
+
+public class SlideItem  {
+
+}
