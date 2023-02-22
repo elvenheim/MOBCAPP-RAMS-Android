1385c702e2d50a1742f65742f70b97d00760ffd9
MindBreak11 <johnmavric11@gmail.com>
2023-02-22 20:37:28 +0800

    marj update

--- app/src/main/java/com/example/ramsdroid/RAMSLogin.java
+++ app/src/main/java/com/example/ramsdroid/RAMSLogin.java
@@ -3,6 +3,8 @@ package com.example.ramsdroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
 
@@ -11,10 +13,23 @@ public class RAMSLogin extends AppCompatActivity {
     EditText email, pass;
     Button btn;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rams_login);
+
+        email = findViewById(R.id.loginEmail);
+        pass = findViewById(R.id.loginPassword);
+        btn = findViewById(R.id.login_btnSignIn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
