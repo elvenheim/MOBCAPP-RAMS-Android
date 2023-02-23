191940835652816b854c49923000aedd5b736442
Richmond <rich.custodio35@gmail.com>
2023-02-23 14:41:42 +0800

    test 2/20/2023 2:52 pm

--- app/src/main/java/com/example/ramsdroid/StudentProfile.java
+++ app/src/main/java/com/example/ramsdroid/StudentProfile.java
@@ -1,13 +1,14 @@
 package com.example.ramsdroid;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class StudentProfile extends Activity {
 
-public class StudentProfile extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rams_student_profile);
     }
+
 }
