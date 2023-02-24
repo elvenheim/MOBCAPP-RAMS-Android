afbdc09c4f0ec41468f6fce1afbd75426c6bbbf1
Richmond <rich.custodio35@gmail.com>
2023-02-24 18:14:04 +0800

    okaneblanco commit and pushing

--- app/src/main/java/com/example/ramsdroid/StudentFinance.java
+++ app/src/main/java/com/example/ramsdroid/StudentFinance.java
@@ -24,4 +24,16 @@ public class StudentFinance extends Activity {
             }
         });
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 }
