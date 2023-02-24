884754b28bf6a1aef5774ae97fa3b108c4f95a32
Richmond <rich.custodio35@gmail.com>
2023-02-24 17:16:07 +0800

    okaneblanco commit and pushing

--- app/src/main/java/com/example/ramsdroid/StudentRegistration.java
+++ app/src/main/java/com/example/ramsdroid/StudentRegistration.java
@@ -23,4 +23,16 @@ public class StudentRegistration extends Activity {
             }
         });
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 }
