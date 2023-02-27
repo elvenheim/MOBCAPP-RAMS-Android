1684e581c8121fd714e62ad5445b9be0c8040960
kin10101 <ijoacquin@gmail.com>
2023-02-27 18:34:58 +0800

    initialized other classes

---
+++ app/src/main/java/com/example/ramsdroid/StudentRec_ClassSched.java
@@ -0,0 +1,14 @@
+package com.example.ramsdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentRec_ClassSched extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rams_studentrec_class_sched);
+    }
+}
