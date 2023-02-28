278cf27c812eca80216416ffc4d2e308d2753b77
kin10101 <ijoacquin@gmail.com>
2023-02-28 21:50:31 +0800

    student record classes init

---
+++ app/src/main/java/com/example/ramsdroid/Records_Grades.java
@@ -0,0 +1,14 @@
+package com.example.ramsdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Records_Grades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rams_records_grades);
+    }
+}
